d3362473e60502f77519fd10a716465af49d16d0
Ali Akbar Mahbadi <aamahbadi@yahoo.com>
2024-12-04 19:37:51 +0330

    update: change default fort of application

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,7 @@ func LoadConfig() *Config {
 
 	metricsPort := os.Getenv("METRICS_PORT")
 	if metricsPort == "" {
-		metricsPort = "9090"
+		metricsPort = "8080"
 	}
 
 	buckets := []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0}
